180f04e94b0f7c3cb0c93b919e24eaee418c7152
unique011 <harshitsharma.2021cs@technonjr.org>
2023-09-23 00:54:07 +0530

    Carosal Data object

---
+++ src/Data/Data.jsx
@@ -0,0 +1,16 @@
+import carosal_1 from "../Assests/carosal1.png";
+import carosal_2 from "../Assests/carosal2.png";
+export const CarouselData = [
+  {
+    Img: carosal_2,
+    Title: "Surface Laptop 5",
+    Body: `Sophisticated style and multitasking speed powered by 12th Gen Intel® Core, with Windows 11`,
+    Link: `/`,
+  },
+  {
+    Img: carosal_1,
+    Title: `Surface Pro 9`,
+    Body: `Tablet versatility and laptop power — all in one ultra-portable device`,
+    Link: `/`,
+  },
+];
