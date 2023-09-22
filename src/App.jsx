fe10633d4299ed367c599283dd0631b7138b71a9
unique011 <harshitsharma.2021cs@technonjr.org>
2023-09-23 00:53:31 +0530

    Create React Route Dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import DashBoard from "./Pages/DashBoard";
+import Home from "./Pages/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/dashbord" element={<DashBoard />}></Route>
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
