f60955d034e9f97af556d2ba727796795fc5ff9d
Ayusht777 <talesarayush@gmail.com>
2023-09-22 22:44:45 +0530

    css update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        bg :"#F5F6F8",
+        primary :"#FFFFFF",
+        accent : "#FDA36F",
+        text:"#061F25",
+
+      }
+    },
   },
   plugins: [],
 };
